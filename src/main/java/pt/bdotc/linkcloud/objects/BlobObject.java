f9ca89b7325ea964a0e5b2568a1ec7a796c10a5b
Gonçalo Lourenço <gmdlourenco@gmail.com>
2015-05-26 18:47:26 +0100

    Blob listing is now implemented. The result given to the client is an XML file.

--- src/main/java/pt/bdotc/linkcloud/objects/BlobObject.java
+++ src/main/java/pt/bdotc/linkcloud/objects/BlobObject.java
@@ -2,14 +2,24 @@ package pt.bdotc.linkcloud.objects;
 
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.blob.*;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotFoundException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
@@ -35,6 +45,8 @@ import java.security.InvalidKeyException;
 @Stateless
 public class BlobObject
 {
+    private static final String XMLROOT= "blobs";
+
     private static CloudStorageAccount
     initStorageConnection(String username, String password)
     throws BadRequestException, InternalServerErrorException
@@ -89,17 +101,20 @@ public class BlobObject
     downloadBlob(String provider, String containerName, String blobName, String username, String password)
     throws BadRequestException, InternalServerErrorException, NotFoundException
     {
+    // Setup access to container
         CloudStorageAccount azureAccount= initStorageConnection(username, password);
-        CloudBlobClient blobClient = azureAccount.createCloudBlobClient();
-        CloudBlobContainer container = initBlobContainer(blobClient, containerName);
+        CloudBlobClient     blobClient= azureAccount.createCloudBlobClient();
+        CloudBlobContainer  container= initBlobContainer(blobClient, containerName);
 
         try
         {
+        // Download blob
             CloudBlockBlob blockBlob= container.getBlockBlobReference(blobName);
             return blockBlob.openInputStream();
         }
         catch (StorageException blobError)
         {
+        // 400 for invalid name; 404 for missing; anything else is error
             int httpStatusCode= blobError.getHttpStatusCode();
 
             if     (httpStatusCode== 400) {throw new BadRequestException("Invalid blob name " + blobName);}
@@ -108,6 +123,7 @@ public class BlobObject
         }
         catch (URISyntaxException e)
         {
+        // Should never happen
             throw new InternalServerErrorException("Error encountered when parsing blob " + blobName);
         }
     }
@@ -116,12 +132,78 @@ public class BlobObject
     public static void
     uploadBlob(String provider, String containerName, String blobName, String username, String password,
                InputStream blobContents, long blobSize)
+    throws BadRequestException, InternalServerErrorException, NotFoundException
     {
+    // Setup access to container
         CloudStorageAccount azureAccount= initStorageConnection(username, password);
-        CloudBlobClient blobClient = azureAccount.createCloudBlobClient();
-        CloudBlobContainer container = initBlobContainer(blobClient, containerName);
+        CloudBlobClient     blobClient= azureAccount.createCloudBlobClient();
+        CloudBlobContainer  container= initBlobContainer(blobClient, containerName);
 
 
+    }
+
+    @SuppressWarnings("unused")
+    public static InputStream
+    listBlobs(String provider, String containerName, String username, String password)
+    throws BadRequestException, InternalServerErrorException, NotFoundException
+    {
+    // Setup access to container
+        CloudStorageAccount azureAccount= initStorageConnection(username, password);
+        CloudBlobClient     blobClient= azureAccount.createCloudBlobClient();
+        CloudBlobContainer  container= initBlobContainer(blobClient, containerName);
+
+        try
+        {
+        // Prepare XML building
+            DocumentBuilderFactory xmlFactory= DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder= xmlFactory.newDocumentBuilder();
+            Document doc= docBuilder.newDocument();
+            doc.setXmlStandalone(true);
+            long blobCounter= 0;
+
+        // Create root element
+            Element rootElement= doc.createElement(XMLROOT);
+            rootElement.setAttribute("count", Long.toString(blobCounter));
+            doc.appendChild(rootElement);
+
+        // Iterate through all blobs
+            for(ListBlobItem blobItem : container.listBlobs())
+            {
+                if(blobItem instanceof CloudBlob)
+                {
+                // Get blob metadata
+                    CloudBlob blob= (CloudBlob) blobItem;
+                    String blobName= blob.getName();
+                    long blobSize= blob.getProperties().getLength();
+
+                    Element blobEntry= doc.createElement("blob");
+                    blobEntry.setAttribute("name", blobName);
+                    blobEntry.setAttribute("size", Long.toString(blobSize));
+
+                // Create blob entry and update blob count in root element
+                    blobCounter++;
+                    rootElement.appendChild(blobEntry);
+                    rootElement.setAttribute("count", Long.toString(blobCounter));
+                }
+            }
+
+        // Prepare XML file output
+            TransformerFactory transformerFactory= TransformerFactory.newInstance();
+            Transformer transformer= transformerFactory.newTransformer();
+            DOMSource source= new DOMSource(doc);
+
+        // Output XML file to memory
+            ByteArrayOutputStream outStream= new ByteArrayOutputStream();
+            Result result= new StreamResult(outStream);
+            transformer.transform(source, result);
+
+        // Return InputStream to XML file
+            return new ByteArrayInputStream(outStream.toByteArray());
+        }
+        catch (Exception exception)
+        {
+            throw new InternalServerErrorException("Error generating XML file for container " + containerName);
+        }
     }
 
     // container.createIfNotExists();
--- src/main/java/pt/bdotc/linkcloud/resources/RequestResource.java
+++ src/main/java/pt/bdotc/linkcloud/resources/RequestResource.java
@@ -35,20 +35,26 @@ public class RequestResource
     private static final Set<String> providersSet= new HashSet<>(Arrays.asList(blobStoreProviders));
 
     private String[]
-    getUsernamePassword(HttpHeaders headers)
-    throws ForbiddenException, BadRequestException
+    getCredentialsValidateCSP(HttpHeaders headers, String provider)
+    throws ForbiddenException, BadRequestException, NotSupportedException
     {
+    // Get Authorization HTTP header
         List<String> authHeadersList= headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
         if(authHeadersList.isEmpty()) {throw new ForbiddenException();}
         String authHeader= authHeadersList.get(0);
 
+    // Make sure authorization is of the Basic type
         String authType= authHeader.split(" ")[0];
         if(!authType.equals("Basic")) {throw new BadRequestException();}
 
+    // Decode header and get username and password from it
         String authDigest= authHeader.substring("Basic ".length());
         java.util.Base64.Decoder decoder= java.util.Base64.getDecoder();
         String[] result= new String(decoder.decode(authDigest)).split(":");
         if(result.length!= 2 || result[0].equals("") || result[1].equals("")) {throw new BadRequestException();}
+
+    // Make sure client is using valid CSP
+        if(!providersSet.contains(provider)) {throw new NotSupportedException();}
 
         return result;
     }
@@ -61,16 +67,13 @@ public class RequestResource
             @PathParam("provider")  String provider,
             @PathParam("container") String container,
             @PathParam("blob")      String blob)
-    throws NotFoundException, ForbiddenException, InternalServerErrorException, NotSupportedException,
-           BadRequestException
+    throws ForbiddenException, BadRequestException, NotSupportedException, NotFoundException,
+           InternalServerErrorException
     {
     // Get username and password from HTTP AUTHORIZATION header
-        String[] credentials= getUsernamePassword(headers); // Throws NotAuthorizedException, BadRequestException.
+        String[] credentials= getCredentialsValidateCSP(headers, provider); // Throws ForbiddenException, BadRequestException
         String username= credentials[0];
         String password= credentials[1];
-
-    // Make sure client is using valid CSP
-        if(!providersSet.contains(provider)) {throw new NotSupportedException();}
 
     // Perform download of blob and return it to client
         /* Throws NotFoundException and InternalServerErrorException. */
@@ -87,20 +90,37 @@ public class RequestResource
             @FormDataParam("blob_id")   String blob,
             @FormDataParam("blob_data") InputStream blobContents,
             @FormDataParam("blob_data") FormDataContentDisposition blobDetails)
-    throws NotAuthorizedException, NotSupportedException
+    throws ForbiddenException, BadRequestException, NotSupportedException
     {
     // Get username and password from HTTP AUTHORIZATION header
-        String[] credentials= getUsernamePassword(headers); // Throws NotAuthorizedException, BadRequestException.
+        String[] credentials= getCredentialsValidateCSP(headers, provider);
         String username= credentials[0];
         String password= credentials[1];
-
-    // Make sure client is using valid CSP
-        if(!providersSet.contains(provider)) {throw new NotSupportedException();}
 
     // Get blob size and try to upload blob
         long blobSize= blobDetails.getSize();
         BlobObject.uploadBlob(provider, container, blob, username, password, blobContents, blobSize);
     }
+
+    @GET
+    @Path("{provider}/{container}")
+    @Produces(MediaType.APPLICATION_XML)
+    public InputStream
+    listBlobs(@Context                    HttpHeaders headers,
+              @PathParam("provider")      String provider,
+              @PathParam("container")     String container)
+    throws ForbiddenException, BadRequestException, NotSupportedException, InternalServerErrorException,
+           NotFoundException
+    {
+    // Get username and password from HTTP AUTHORIZATION header
+        String[] credentials= getCredentialsValidateCSP(headers, provider);
+        String username= credentials[0];
+        String password= credentials[1];
+
+    // Get XML list and return it
+        return BlobObject.listBlobs(provider, container, username, password);
+    }
+
 
     // Function to @GET a blob listing (return XML file)
     // Function to @DELETE blob
